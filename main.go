59ee8637153254f554e7dddc70d58d17b4dea691
Özgür Yalçın <yalcin.ozqur@gmail.com>
2021-01-23 02:35:03 +0300

    Update main.go

--- main.go
+++ main.go
@@ -8,19 +8,19 @@ import (
 )
 
 func main() {
-	api := &posnet.API{"yapikreditest"} // "yapikredi"
+	api := &posnet.API{"yapikreditest"} // "yapikredi","yapikreditest"
 	request := new(posnet.Request)
 	request.MerchantID = "6706598320"
 	request.TerminalID = "67005551"
 	request.TranDate = "1"
 	request.Sale = new(posnet.Sale)
-	request.Sale.Amount = "2451"                     // Satış tutarı (1,00 -> 100) Son 2 hane kuruş
-	request.Sale.CurrencyCode = "TL"                 // TL, US, EU
-	request.Sale.CardNumber = "4506349116608409"     // Kart numarası
-	request.Sale.ExpireDate = "0703"                 // Son kullanma tarihi (Yıl ve ayın son 2 hanesi) YYAA
-	request.Sale.CVV2 = "000"                        // Cvv2 Kodu (kartın arka yüzündeki 3 haneli numara)
-	request.Sale.Installment = "00"                  // peşin: "00", 2 taksit: "02"
-	request.Sale.OrderID = "1s3456z8901234567890123" // Sipariş numarası
+	request.Sale.Amount = "2451"                 // Satış tutarı (1,00 -> 100) Son 2 hane kuruş
+	request.Sale.CurrencyCode = "TL"             // TL, US, EU
+	request.Sale.CardNumber = "4506349116608409" // Kart numarası
+	request.Sale.ExpireDate = "0703"             // Son kullanma tarihi (Yıl ve ayın son 2 hanesi) YYAA
+	request.Sale.CVV2 = "000"                    // Cvv2 Kodu (kartın arka yüzündeki 3 haneli numara)
+	request.Sale.Installment = "00"              // peşin: "00", 2 taksit: "02"
+	request.Sale.OrderID = ""                    // Sipariş numarası
 	response := api.Transaction(request)
 	pretty, _ := xml.MarshalIndent(response, " ", " ")
 	fmt.Println(string(pretty))
